d4a3b51e8db20aecafdfa338a6c12e2ced4c12cd
MAKOSCAFEE <barnabasmakonda@gmail.com>
2017-08-08 20:58:52 +0300

    started using ui-route instead of ng-router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,18 +16,28 @@ import {StoreDevtoolsModule} from '@ngrx/store-devtools';
 import { SharedModule } from './shared/shared.module';
 import { ServicesModule } from './services/services.module';
 import { ComponentsModule } from './components/components.module';
+import { AppRoutingModule } from './app.routing';
+
 
 //components
 import { HomeComponent } from './home/home.component';
 import { OrgUnitFilterComponent } from  './components/org-unit-filter/org-unit-filter.component';
 import { MultiselectComponent } from './components/org-unit-filter/multiselect/multiselect.component';
+import { ActionComponent } from './home/action/action.component';
+import { OrgUnitsComponent } from './home/org-units/org-units.component';
+import { DataSetsComponent } from './home/data-sets/data-sets.component';
+import { ResultsComponent } from './home/results/results.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     MultiselectComponent,
-    OrgUnitFilterComponent
+    OrgUnitFilterComponent,
+    ActionComponent,
+    OrgUnitsComponent,
+    DataSetsComponent,
+    ResultsComponent
   ],
   imports: [
     BrowserModule,
@@ -36,6 +46,7 @@ import { MultiselectComponent } from './components/org-unit-filter/multiselect/m
     SharedModule,
     ServicesModule,
     ComponentsModule,
+    AppRoutingModule,
     TreeModule,
     StoreModule.provideStore({uiState: uiStateReducer,storeData: storeDataReducer},INITIAL_APPLICATION_STATE),
     StoreDevtoolsModule.instrumentOnlyWithExtension()
---
+++ src/app/app.routing.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, HashLocationStrategy, Location, LocationStrategy } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
+import { UIRouterModule } from "@uirouter/angular";
+import { Routes, RouterModule, PreloadAllModules} from '@angular/router';
+
+
+import { HomeComponent } from './home/home.component';
+import { ActionComponent } from './home/action/action.component';
+import { OrgUnitsComponent } from './home/org-units/org-units.component';
+import { DataSetsComponent } from './home/data-sets/data-sets.component';
+import { ResultsComponent } from './home/results/results.component';
+
+import { UIRouterConfigFn }   from "./app.router";
+
+
+
+const appStates = [
+  { name: 'home', url: '', component: HomeComponent },
+  { name: 'home.action', url: '/action', component: ActionComponent },
+  { name: 'home.data-sets', url: '/data-sets', component: DataSetsComponent },
+  { name: 'home.org-units', url: '/org-units', component: OrgUnitsComponent },
+  { name: 'home.results', url: '/results', component: ResultsComponent },
+];
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    UIRouterModule.forRoot({
+      states: appStates,
+      useHash: true,
+      config: UIRouterConfigFn
+    })
+  ],
+  declarations: [],
+  providers: [Location, { provide: LocationStrategy, useClass: HashLocationStrategy }],
+  exports: [
+    UIRouterModule
+  ]
+})
+export class AppRoutingModule { }
