45a40a020c0ba3173588998acc12962b272cf52a
MAKOSCAFEE <barnabasmakonda@gmail.com>
2017-09-25 14:24:15 +0300

    Add optional add usergroups

--- src/app/pages/create-users/create-users.component.ts
+++ src/app/pages/create-users/create-users.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/user.model';
 import { matchOtherValidator } from '../../shared/match-other-validator'
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { SharedDataService } from '../../shared/shared-data.service';
 import { ToastService } from '../../services/toast.service';
+import { UserService } from '../../services/user.service';
 import { DataStoreService } from '../../services/data-store.service';
 import { MessageConversationService } from '../../services/message-conversation.service';
+import { IMultiSelectOption, IMultiSelectSettings, IMultiSelectTexts } from 'angular-2-dropdown-multiselect';
 
 import * as _ from 'lodash';
 import * as async from 'async-es';
@@ -19,40 +21,64 @@ export class CreateUsersComponent implements OnInit {
 
   public selectedOrgUnitNames: String[];
   public selectedOrgUnitIDs: any;
+  public allUserGroups: any;
   public isOrganizationUnitSelected: boolean = false;
-  private randomGeneratedID:string;
+  private randomGeneratedID: string;
   private feedbackRecipients: any;
   public userDetails: FormGroup;
 
   public orgunit_tree_config: any = {
-    show_search : true,
-    search_text : 'Search',
+    show_search: true,
+    search_text: 'Search',
     level: null,
     loading: true,
     loading_message: 'Loading Organisation units...',
     multiple: true,
-    multiple_key:"none", //can be control or shift
+    multiple_key: "none", //can be control or shift
     placeholder: "Select Organisation Unit"
   };
 
-  public orgunit_model: any =  {
+  public orgunit_model: any = {
     selection_mode: "orgUnit",
     selected_level: "",
-    show_update_button:true,
+    show_update_button: true,
     selected_group: "",
     orgunit_levels: [],
     orgunit_groups: [],
     selected_orgunits: [],
     user_orgunits: [],
-    type:"report", // can be 'data_entry'
+    type: "report", // can be 'data_entry'
     selected_user_orgunit: "USER_ORGUNIT"
+  };
+
+  // Settings configuration
+  public mySettings: IMultiSelectSettings = {
+    enableSearch: true,
+    buttonClasses: 'btn btn-secondary btn-sm btn-block',
+    containerClasses: 'dropdown-block',
+    dynamicTitleMaxItems: 6,
+    displayAllSelectedText: true
+  };
+
+  // Text configuration
+  public myTexts: IMultiSelectTexts = {
+    checkAll: 'Select all',
+    uncheckAll: 'Unselect all',
+    checked: 'userGrops selected',
+    checkedPlural: 'userGroups selected',
+    searchPlaceholder: 'Find',
+    searchEmptyResult: 'Nothing found...',
+    searchNoRenderText: 'Type in search box to see results...',
+    defaultTitle: 'Select user groups user belongs',
+    allSelected: 'All selected',
   };
 
 
   constructor(private _dataStoreService: DataStoreService,
-              private _sharedDataService: SharedDataService,
-              private _toastService: ToastService,
-              private _messageConversationService: MessageConversationService) { }
+    private _sharedDataService: SharedDataService,
+    private _toastService: ToastService,
+    private _messageConversationService: MessageConversationService,
+    private _userService: UserService) { }
 
   ngOnInit() {
 
@@ -61,13 +87,18 @@ export class CreateUsersComponent implements OnInit {
       surname: new FormControl('', [Validators.required, Validators.minLength(2)]),
       email: new FormControl('', [Validators.minLength(2)]),
       phoneNumber: new FormControl('', [Validators.minLength(2)]),
+      userGroups: new FormControl(''),
       userCredentials: new FormGroup({
         username: new FormControl('', [Validators.required, Validators.minLength(2)]),
         password: new FormControl('', [Validators.required, Validators.minLength(8)]),
-        confirm: new FormControl('', [Validators.required, Validators.minLength(8),matchOtherValidator('password')])
+        confirm: new FormControl('', [Validators.required, Validators.minLength(8), matchOtherValidator('password')])
       })
     });
 
+    /**
+     * [asyc.parallel parallel call multiple request]
+     * @param  {[function]} [(callback [callback function]
+     */
     async.parallel([
       (callback) => {
         this._sharedDataService.getFeedbackReceipients().subscribe(response => {
@@ -78,52 +109,83 @@ export class CreateUsersComponent implements OnInit {
         this._sharedDataService.getRandomGeneratedId().subscribe(response => {
           callback(null, response)
         })
+      },
+      (callback) => {
+        this._userService.getAllUserGroups().subscribe(response => {
+          callback(null, response)
+        })
       }
     ], (error, results) => {
       this.feedbackRecipients = results[0];
-      this.randomGeneratedID = results[1]
-      console.log(results[1])
-
+      this.randomGeneratedID = results[1];
+      this.allUserGroups = _.transform(results[2].userGroups, (results, userGroup) => {
+        results.push({
+          id: userGroup.id,
+          name: userGroup.displayName
+        })
+      }, []);
     })
   }
 
-  setSelectedOrgunit(event){
-    this.selectedOrgUnitIDs = _.transform(event.value,(results,value) => {
-      results.push(_.pick(value,'id'));
-    },[]);
+  /**
+   * [setSelectedOrgunit set all selected organisationUnits]
+   * @param  {[Any]} event [description]
+   * @return {[type]}       [description]
+   */
+  setSelectedOrgunit(event) {
+    this.selectedOrgUnitIDs = _.transform(event.value, (results, value) => {
+      results.push(_.pick(value, 'id'));
+    }, []);
 
     this.selectedOrgUnitNames = _.map(event.value, 'name');
 
-    if(event.value.length >= 1){
+    if (event.value.length >= 1) {
       this.isOrganizationUnitSelected = true;
     }
 
   }
 
-  createDataStoreObjKey(){
-
+  /**
+   * [createDataStoreObjKey generate random 62-bits key]
+   * @return {[string]} [generated random key]
+   */
+  createDataStoreObjKey() {
     let formatter = new Intl.DateTimeFormat("fr", { month: "short" }),
-        month = formatter.format(new Date()),
-        text = '',
-        possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      month = formatter.format(new Date()),
+      text = '',
+      possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-    for(let i=0; i < 3; i++){
+    for (let i = 0; i < 3; i++) {
       text += possible.charAt(Math.floor(Math.random() * possible.length));
     }
 
-    return month.slice(0, -1).toUpperCase().concat('_',text);
+    return month.slice(0, -1).toUpperCase().concat('_', text);
 
   }
 
+  /**
+   * [onSubmit description]
+   * @param  {[type]}     {value [description]
+   * @param  {User}       valid} [description]
+   * @param  {boolean }}    valid         [description]
+   * @return {[type]}            [description]
+   */
   onSubmit({ value, valid }: { value: User, valid: boolean }) {
-    let userPayload = Object.assign({},value);
+    let userPayload = Object.assign({}, value);
     userPayload.organisationUnits = this.selectedOrgUnitIDs;
     userPayload.id = this.randomGeneratedID;
+    userPayload.userGroups = _.transform(value.userGroups, (result, userGroupID) => {
+      if (userGroupID.length) {
+        result.push({
+          id: userGroupID
+        })
+      };
+    },[]);
 
     userPayload.userCredentials.userInfo = {
       id: this.randomGeneratedID
     }
-
+    console.log(userPayload);
     let dataStoreKey = this.createDataStoreObjKey();
     let datasetUrlTosendTo = `api/users`;
     let payload = [{
@@ -137,27 +199,33 @@ export class CreateUsersComponent implements OnInit {
     let feedbackSubject = `${dataStoreKey}:REQUEST FOR CREATING USER`;
     let text = `There is request to create user to ${this.selectedOrgUnitNames} orgnisation unit, with the following information`;
     this._dataStoreService
-        .createNewKeyAndValue(dataStoreKey,payload)
-        .subscribe(response =>{
+      .createNewKeyAndValue(dataStoreKey, payload)
+      .subscribe(response => {
 
-          if(response.ok){
-            this._toastService.success('Your changes were sent for approval, Thanks.')
-            this.sendFeedBackMessage(feedbackSubject, text);
+        if (response.ok) {
+          this._toastService.success('Your changes were sent for approval, Thanks.')
+          this.sendFeedBackMessage(feedbackSubject, text);
 
-          } else {
-            this._toastService.error('There was an error when sending data.')
+        } else {
+          this._toastService.error('There was an error when sending data.')
 
-          }
-        });
+        }
+      });
   }
 
-  sendFeedBackMessage(subject,message){
+  /**
+   * [sendFeedBackMessage description]
+   * @param  {string} subject [description]
+   * @param  {string} message [description]
+   * @return {[type]}         [description]
+   */
+  sendFeedBackMessage(subject: string, message: string) {
     let payload = {
       subject: subject,
       text: message,
-      userGroups: [{id: this.feedbackRecipients.id}]
+      userGroups: [{ id: this.feedbackRecipients.id }]
     }
-    this._messageConversationService.sendFeedBackMessage(payload).subscribe(response =>{
+    this._messageConversationService.sendFeedBackMessage(payload).subscribe(response => {
       // TODO: Send notification if possible about new message.
       //console.log(response);
 
