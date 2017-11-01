18b53bc574abfcc49900e846bd7773209b4255b0
Barnabas Makonda <barnabasmakonda@gmail.com>
2017-11-01 21:25:28 +0300

    refactor dataset select

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrgUnitService } from './org-unit-filter/org-unit.service';
+import { DatasetSelectComponent } from './add-form/dataset-select/dataset-select.component';
 
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [],
+  declarations: [DatasetSelectComponent],
+  exports: [DatasetSelectComponent],
   providers: [
     OrgUnitService
   ]
